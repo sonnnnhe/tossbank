c694dc1861868bb13f90e3ddc3d411f7a9c26575
sonnnnhe <fladi39@naver.com>
2025-02-18 11:12:49 +0900

    push

---
+++ nodejs/basics/ex03-arrow-function.js
@@ -0,0 +1,34 @@
+// 일반함수와 화살표함수(ES6 - ES2015)
+
+//함수형 변수(함소 표현식)
+let hi = function () {
+  //익명함수
+  return '안녕하세요.'
+}
+console.log(hi())
+
+//일반함수의 매개변수로 전달
+function sayHi(method) {
+  //고차함수
+  console.log(method())
+}
+sayHi(hi)
+
+//화살표함수로 변경
+let hi2 = () => {
+  return '안녕하세요'
+}
+sayHi(hi2)
+
+let sum = (a, b) => {
+  return a + b
+}
+console.log(sum(10, 20))
+
+//화살표함수의 수행문이 한줄이면, 중괄호, return 생략 가능
+let sub = (a, b) => a - b
+console.log(sub(10, 20))
+
+//매개변수 하나이면, 소괄호 생략 가능
+let double = (a) => a * 2
+console.log(double(10))
