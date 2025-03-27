36612662f4d6b1358402416b5de9b062d166a7b2
sonnnnhe <fladi39@naver.com>
2025-03-27 16:29:33 +0900

    프로젝트 초기 설정

---
+++ nextjs/weather-app/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+// 메타데이터 - 창에 뜨는 제목 변경 가능
+export const metadata: Metadata = {
+  title: '날씨 앱',
+  description: '날씨를 알려주는 앱입니다',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  )
+}
