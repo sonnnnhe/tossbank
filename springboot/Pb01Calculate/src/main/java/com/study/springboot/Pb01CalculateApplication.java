d28e4cec9ff1d1a99bb5a96af2d808d43ca24e83
sonnnnhe <fladi39@naver.com>
2025-04-04 16:10:07 +0900

    update

---
+++ springboot/Pb01Calculate/src/main/java/com/study/springboot/Pb01CalculateApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pb01CalculateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pb01CalculateApplication.class, args);
+	}
+
+}
