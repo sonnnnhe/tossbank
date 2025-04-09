a4b03ddfb03e6654a4220514165e7ac153e6b79e
sonnnnhe <fladi39@naver.com>
2025-04-09 17:50:44 +0900

    update

---
+++ springboot/Pr04CounterAPI/src/test/java/com/study/springboot/Pr04CounterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr04CounterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
