10e52846057a4bd81a66d4db05eb2b9edde35970
sonnnnhe <fladi39@naver.com>
2025-04-23 10:15:42 +0900

    feat: 게시글 조회

--- springboot/Practice/src/main/java/com/study/springboot/controller/ArticleController.java
+++ springboot/Practice/src/main/java/com/study/springboot/controller/ArticleController.java
@@ -3,11 +3,17 @@ package com.study.springboot.controller;
 import com.study.springboot.dto.ArticleForm;
 import com.study.springboot.entity.Article;
 import com.study.springboot.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired
@@ -18,15 +24,47 @@ public class ArticleController {
         return "articles/new";
     }
 
+    // 게시글 작성
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
+        // System.out.println(form.toString());
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+        // System.out.println(article.toString());
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        // System.out.println(saved.toString());
         return "";
     }
+
+    // 게시글 단건 조회
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // Optional<Article> articleEntity = articleRepository.findById(id);
+
+        // 2. name이라는 이름으로 value 객체 추가
+        model.addAttribute("article", articleEntity);
+
+        return "articles/show";
+    }
+
+    // 게시글 목록 조회
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        return "articles/index";
+    }
+
+
 }
--- springboot/Practice/src/main/java/com/study/springboot/repository/ArticleRepository.java
+++ springboot/Practice/src/main/java/com/study/springboot/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package com.study.springboot.repository;
 import com.study.springboot.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
