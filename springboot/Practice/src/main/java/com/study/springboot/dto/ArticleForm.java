0400810568581b3c38cf82fa497cf06816ce51e1
sonnnnhe <fladi39@naver.com>
2025-04-23 10:14:45 +0900

    refactor: lombok 사용

--- springboot/Practice/src/main/java/com/study/springboot/dto/ArticleForm.java
+++ springboot/Practice/src/main/java/com/study/springboot/dto/ArticleForm.java
@@ -1,27 +1,15 @@
 package com.study.springboot.dto;
 
 import com.study.springboot.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 // ArticleDTO
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title;  // 제목
     private String content;  // 내용
-
-    // 마우스 우클릭 - 생성 - 생성자
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    // 마우스 우클릭 - 생성- toString()
-    // 폼 데이터를 잘 받았는지 확인
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     // Entity로 변환하는 함수
     public Article toEntity() {
